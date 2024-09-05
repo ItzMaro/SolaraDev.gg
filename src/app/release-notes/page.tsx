dc4d8b0ed404233419db65cbed5922e11d1d473a
ItzMaro <139705078+ItzMaro@users.noreply.github.com>
2024-09-05 10:18:29 -0700

    Update page.tsx

--- src/app/release-notes/page.tsx
+++ src/app/release-notes/page.tsx
@@ -7,5 +7,7 @@ import Link from "next/link";
 
 export default function ReleaseNotes() {
   return (
+    <main className="flex min-h-screen flex-col items-center justify-start">
+    </main>
   )
 }
