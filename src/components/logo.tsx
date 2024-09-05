dbf98b246c961e6bf884c2cbed548727ca57bae9
ItzMaro <139705078+ItzMaro@users.noreply.github.com>
2024-09-05 05:42:34 -0700

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -9,7 +9,7 @@ export default function Logo({ withText, ...props }: any) {
   return (
     <div className="flex items-center m-0" {...props}>
       <CachedImage src={`www/public/logos/zen-black.svg`} width={40} height={40} alt="Zen Logo" className={ny("transition-all duration-300 hover:scale-110", withText && "mr-2")} />
-      {withText && <span className="text-2xl font-bold ml-2">zen</span>}
+      {withText && <span className="text-2xl font-bold ml-2">solara</span>}
     </div>
   );
 }
