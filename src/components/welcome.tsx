0acf6fcab7e3c46147e660f0b08603e6415da738
ItzMaro <139705078+ItzMaro@users.noreply.github.com>
2024-09-05 05:40:37 -0700

    Update welcome.tsx

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -5,8 +5,8 @@ import WordPullUp from "./ui/word-pull-up";
 export default function WelcomePage() {
   return (
     <div className="w-full relative min-h-screen flex flex-col items-center justify-center">
-      <WordPullUp className="text-6xl text-center" words="Welcome to Zen Browser!" />
-      <p className="max-w-90 text-lg mt-12">A Firefox based browser with a focus on privacy and customization.<br/>Start using it by clicking on the sidebar icon or trying out the split view feature!</p>
+      <WordPullUp className="text-6xl text-center" words="Welcome to Solara Exploit!" />
+      <p className="max-w-90 text-lg mt-12">A Roblox Exploit designed to be best for user experience.<br/>Start using it by clicking attach icon or using the Solara V4 AI Feature!</p>
     </div>
   );
 }
