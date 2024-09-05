ce8536b1b577271550579c566b95c6d0132bd241
ItzMaro <139705078+ItzMaro@users.noreply.github.com>
2024-09-05 10:17:32 -0700

    Update page.tsx

--- src/app/release-notes/page.tsx
+++ src/app/release-notes/page.tsx
@@ -1,1 +1,11 @@
+"use client";
 
+import Footer from "@/components/footer";
+import { Navigation } from "@/components/navigation";
+import { releaseNoteIsAlpha, releaseNotes } from "@/lib/release-notes";
+import Link from "next/link";
+
+export default function ReleaseNotes() {
+  return (
+  )
+}
